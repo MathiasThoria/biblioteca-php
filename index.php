2c8f6847cd2580fda40f5f4c9ab166ee840a13d7
DynamoDreams <52427185+erossosa@users.noreply.github.com>
2025-11-06 22:47:54 -0300

    cositas
    - Añade un nuevo dashboard, login profesional y Navbar.
    - Implementa barras de búsqueda para Libros y Usuarios.
    - Corrige bugs de lógica MVC (redirects, includes).
    - Soluciona bugs de validación (préstamos) y edición (usuarios).
    - Corrige solapamiento de la Navbar fija.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors',1); ini_set('display_startup_errors',1); error_reporting(E_ALL);
+
+define('BASE_URL', 'http://localhost/biblioteca-php-main/');
 
 session_start(); // iniciar sesión
 //$_SESSION = [];
